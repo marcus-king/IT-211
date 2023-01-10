70911e7ec551f57e843fe27bfa69d95183a34881
Marcus King <marcus.king@pm.me>
2023-01-09 19:46:59 -0800

    Initial commit.

---
+++ FavoriteQuote.java
@@ -0,0 +1,7 @@
+class FavoriteQuote{
+	public static void main(String[] args) {
+		String output = "\"Any sufficiently advanced technology is indistinguishable from magic.\"\n\n						     -Arthur C. Clarke";
+		System.out.println(output);
+	}
+}
+	
